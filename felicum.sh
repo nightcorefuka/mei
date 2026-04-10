93379a8d1248d49d7b4ea8a3062b4585653fa54e
fuka <nightcorefuka@gmail.com>
2026-04-09 23:38:14 -0300

    finalentrecomillas

--- felicum.sh
+++ felicum.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 while true; do
-    if [ "09-04" == "$(date +%d:%m)" ]; then
+    if [ "10-04" == "$(date +%d:%m)" ]; then
         termux-vibrate -d 2000
         termux-notification --title "FELIZ CUMEADA" --content "Feliz cumpleaños, Mei, te quierop"
         termux-tts-speak "Feliz cumpleaños, Mei, te quierop"
